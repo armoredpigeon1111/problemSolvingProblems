2338cffa1e03f6c09566431ce7cf21538e77659d
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-08-02 16:48:28 -0400

    Problem Solving Problem 3 - Incrementing Sequence added runIncrementingSequence, sortArray, and checkIncrement.

--- app3.js
+++ app3.js
@@ -36,4 +36,87 @@ function getUserArray(){
     return userArray;
 }
 
-runTargetNum();
+//runTargetNum();
+
+
+
+//PROBLEM 2 - Palindrome
+
+function runPalindromeChecker(){
+    console.log("PROBLEM 4: Palindrome Checker."); //printing title to console
+
+    let userString = prompt("Please enter a string to check if it is a palindrome: ");
+    let stringReversed = reverseString(userString);
+    let stringIsPanlindrome = checkPalindrome(userString, stringReversed);
+
+    console.log("String is a palindrome: " + stringIsPanlindrome);
+
+}
+
+
+function reverseString(enteredString){
+    let reversedString = "";
+
+    for(let i = enteredString.length -1; i >= 0; i--){
+        reversedString += enteredString[i];
+    }
+
+    return reversedString;
+}
+
+
+function checkPalindrome(userString, reversedString){
+    let isPalindrome = true;
+
+    for(let i = 0; i < userString.length; i++){
+        if(userString[i] != reversedString[i]){
+            isPalindrome = false;
+        }
+    }
+
+    return isPalindrome;
+
+}
+
+
+//runPalindromeChecker();
+
+
+
+
+//PROBLEM 3 - Incrementing Sequence
+
+function runIncrementingSequence(){
+    let userSequence = getUserArray();
+    let sortedArray = sortArray(userSequence);
+    let sequenceIncrementing = checkIncrement(sortedArray);
+
+    if(sequenceIncrementing){
+        console.log(`The sequence ${sortedArray} is an incrementing sequence.`);
+    }else{
+        console.log(`The sequence ${sortedArray} is NOT an incrementing sequence.`);
+    }
+}
+
+
+//Sorts array in ascending order
+function sortArray(userArray){           
+    userArray.sort(function(a, b){return a-b});
+    return userArray;
+}
+
+
+//Checks if array increments by 1
+function checkIncrement(sortedArray){
+    let isIncrementingSequence = true;
+
+    for(let i = 0; i < sortedArray.length -1; i++){
+        if(sortedArray[i] + 1 !== sortedArray[i + 1]){
+            return false;
+        }
+    }
+
+    return isIncrementingSequence;
+}
+
+runIncrementingSequence();
