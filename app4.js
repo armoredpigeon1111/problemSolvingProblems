3a39a75a452e7e82d1551b3983b51c1103a5a04b
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-08-03 16:10:56 -0400

    Added runLeapYearFinder, getCurrentYear, checkLeapYear, and calculateLeapYear.

--- app4.js
+++ app4.js
@@ -8,11 +8,40 @@
 //PROBLEM 1 - Next 20 Leap Years
 function runLeapYearFinder(){
     let currentYear = getCurrentYear();
+    let isCurrentlyLeapYear = checkLeapYear(currentYear);
+    let leapYearsArray = [];
+    if(isCurrentlyLeapYear){
+        leapYearsArray = calculateLeapYears(currentYear);
+        console.log(leapYearsArray);
+    }else{
+        let nextLeapYear = getNextLeapYear(year);
+    }
 }
 
 function getCurrentYear(){
     let currentDate = new Date();
     let currentYear = currentDate.getFullYear();
+
+    return currentYear;
 }
+
+function checkLeapYear(year){
+    if(year % 4 === 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function calculateLeapYears(year){
+    let leapYear = year;
+    let leapYearsArray = [];
+
+    while(leapYearsArray.length < 20){
+        leapYear += 4;
+        leapYearsArray.push(leapYear);
+    }
+}
+
 
 runLeapYearFinder();
